72e1f36fac1efea481da605b3ad9c4e9678d1b97
radoslav.boychev <radoslav.boychev@vegait.rs>
2023-02-08 16:34:24 +0100

    caesar cipher complete

--- hacker-rank-caesar-cipher/cmd/caesar-cipher/main.go
+++ hacker-rank-caesar-cipher/cmd/caesar-cipher/main.go
@@ -1,10 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	rest := caesarCipher("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 2)
-	fmt.Println(rest)
+
+	// var delta int
+	// var input string
+	// fmt.Scanln("Delta %d\n", &input)
+	// fmt.Scanln("Input string %d\n", &delta)
+
+	res := caesarCipher("There's a starman waiting in the sky", 3)
+
+	fmt.Println(res)
 }
 
 // caesarCipher shifts each letter by a number of letters
@@ -20,26 +29,15 @@ func caesarCipher(s string, k int32) string {
 
 	// create a new slice for the "encrypted" string
 	var res []rune
-	var i int32 = 0
 	// change the value of each unicode character
 	for _, r := range alphabetSlice {
 
-		// change the value of the unicode character
-		r = r + k
-		i++
+		// // change the value of the unicode character
+		// r = r + k
+
 		// if its larger than the 'Z' character, set it to 'A'
 		// this loops the alphabet
-
-		if r < 65 {
-			r = 65 + i
-		}
-
-		if r >= 91 {
-			r = 65 + i
-		}
-
-		// if its larger than the 'z' character, set it to the 'a'
-		// this loops the alphabet
+		r = cipher(r, k)
 
 		// append the shifted unicode rune to the string
 		res = append(res, r)
@@ -47,4 +45,22 @@ func caesarCipher(s string, k int32) string {
 
 	// return the final string
 	return string(res)
+}
+
+//
+func cipher(r rune, delta int32) rune {
+	if r >= 'A' && r <= 'Z' {
+		return rotate(r, 'A', int(delta))
+	}
+	if r >= 'a' && r <= 'z' {
+		return rotate(r, 'a', int(delta))
+	}
+	return r
+}
+
+//
+func rotate(r rune, base, delta int) rune {
+	tmp := int(r) - base
+	tmp = (tmp + delta) % 26
+	return rune(tmp + base)
 }
